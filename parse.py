934baa657484cc95f7d5226bb9d4f7712d8e4fa6
Paul Infield-Harm <paul@infield-harm.org>
2022-08-15 10:21:25 -0400

    Simplified

--- parse.py
+++ parse.py
@@ -9,23 +9,22 @@ def tokenize(s: str) -> Optional[List[str]]:
 
 
 def parse(tokens: List[str]) -> AST:
-    if tokens[0] != '(' or tokens[-1] != ')':
-        raise SyntaxError("Expected parens")
-
-    scopes = [[]]
-    for t in tokens[1:-1]:
+    scopes = []
+    last_scope = None
+    for t in tokens:
         if t == '(':
             new_scope = []
-            scopes[-1].append(new_scope)
+            if scopes:
+                scopes[-1].append(new_scope)
             scopes.append(new_scope)
         elif t == ')':
-            scopes.pop()
+            last_scope = scopes.pop()
         elif t.isnumeric():
             scopes[-1].append(int(t))
         else:
             scopes[-1].append(t)
 
-    return scopes[0]
+    return last_scope
 
 
 def ast(s: str) -> Optional[AST]:
