5c2f8ddefe42b98755c43a359059fd2845666aa0
Paul Infield-Harm <paul@infield-harm.org>
2022-08-15 10:10:53 -0400

    Working

--- parse.py
+++ parse.py
@@ -1,2 +1,39 @@
+from typing import Union, List, Optional
+import re
+
+AST = Union[str, List['AST']]
+
+
+def tokenize(s: str) -> Optional[List[str]]:
+    return re.findall(r'[()]|[^\s()]+', s)
+
+
+def parse(tokens: List[str]) -> AST:
+    if tokens[0] != '(' or tokens[-1] != ')':
+        raise SyntaxError("Expected parens")
+
+    scopes = [[]]
+    for t in tokens[1:-1]:
+        if t == '(':
+            new_scope = []
+            scopes[-1].append(new_scope)
+            scopes.append(new_scope)
+        elif t == ')':
+            scopes.pop()
+        elif t.isnumeric():
+            scopes[-1].append(int(t))
+        else:
+            scopes[-1].append(t)
+
+    return scopes[0]
+
+
+def ast(s: str) -> Optional[AST]:
+    tokens = tokenize(s)
+    if not tokens:
+        return None
+    return parse(tokens)
+
+
 if __name__ == '__main__':
     print("Write me!")
--- test_parse.py
+++ test_parse.py
@@ -1,5 +1,45 @@
 import pytest
 
+from parse import ast
 
-def test_failure():
-    pytest.fail("Starting")
+
+def test_parse_example():
+    s = "(first (list 1 (+ 2 3) 9))"
+    result = ast(s)
+    assert result == ["first", ["list", 1, ["+", 2, 3], 9]]
+
+
+def test_parse_multiple_sibling_lists():
+    s = "(alpha (beta 1 2) (gamma 3 4))"
+    result = ast(s)
+    assert result == ["alpha", ["beta", 1, 2], ["gamma", 3, 4]]
+
+
+def test_parse_empty():
+    s = ""
+    result = ast(s)
+    assert result is None
+
+
+def test_parse_empty_whitespace():
+    s = "      "
+    result = ast(s)
+    assert result is None
+
+
+def test_empty_list():
+    s = "()"
+    result = ast(s)
+    assert result == []
+
+
+def test_empty_list_embedded():
+    s = "(foo ())"
+    result = ast(s)
+    assert result == ["foo", []]
+
+
+def test_not_a_program():
+    with pytest.raises(SyntaxError):
+        s = "abc 1 2 3"
+        ast(s)
